209fa9b6f5dd3384fce4ac366f2518db67abab70
Min Woo <minwoo@iMac5.local>
2019-02-05 09:24:43 -0500

    exercises completed

--- Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -10,17 +10,18 @@ var secondDecimal = 0.2
  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
 var trueOrFalse = true
-firstDecimal = trueOrFalse
-print("it does not because the type does not match")
+print("it does not because I cannot assign bool to double")
 
 /*:
  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
+var aVariable = "string"
+print("it dose not because I cannot assign string to double")
 
 /*:
  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
+var aWholeNumber = 1
+print("It does not because I cannot assign in to double")
 
 //: [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
