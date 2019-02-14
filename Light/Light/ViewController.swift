600558fb24b681ebd03d3b083ffa3287df5c8476
Min Woo <minwoo@iMac5.local>
2019-02-14 10:15:46 -0500

    Light guided project started

---
+++ Light/Light/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Min Woo on 2/14/19.
+//  Copyright © 2019 Min Woo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var lightButton: UIButton!
+    
+}
+
